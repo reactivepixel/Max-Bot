75a122bae75adb62107887312807dc147753025e
Chris Chapman <Chapman@apextion.com>
2017-08-15 01:21:06 -0400

    base functionality to add / remove roles

--- bot/client.js
+++ bot/client.js
@@ -6,8 +6,41 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-  if (message.content === 'ping') {
-    message.reply('pong');
+  if (message.content.charAt(0) === '!') {
+    // message.reply('pong');
+    //Find out what command was initiated
+    const splitInput = message.content.split(' ');
+
+    // match first command with the database
+    //splitInput[0]
+
+    if(splitInput[0].toLowerCase() === '!addrole'){
+      // Add roll position 2 to user @ position 3
+
+      let targetRole = message.guild.roles.find('name', splitInput[1])
+      if(targetRole === null){
+        return message.reply('"' + splitInput[1] + '" is not a known role.')
+      }
+      let targetMember = message.mentions.members.first();
+
+      targetMember.addRole(targetRole).catch(console.error);
+      message.reply(targetRole.name + ' added to ' + targetMember.user.username);
+    }
+
+    if(splitInput[0].toLowerCase() === '!removerole'){
+      // Add roll position 2 to user @ position 3
+
+      let targetRole = message.guild.roles.find('name', splitInput[1])
+      if(targetRole === null){
+        return message.reply('"' + splitInput[1] + '" is not a known role.')
+      }
+      let targetMember = message.mentions.members.first();
+
+      targetMember.removeRole(targetRole).catch(console.error);
+
+      message.reply(targetRole.name + ' removed from ' + targetMember.user.username);
+    }
+
   }
 });
 
---
+++ bot/models/db.js
@@ -0,0 +1,45 @@
+const Sequelize = require('sequelize');
+
+if (!process.env.NODE_ENV) require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_SCHEMA,
+  port: process.env.DB_PORT,
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000,
+  },
+  logging: false,
+});
+
+
+
+const cmd = sequelize.define('cmd', {
+  cmd: {
+    type: Sequelize.STRING,
+  },
+  regex: {
+    type: Sequelize.STRING,
+  },
+});
+
+const action = sequelize.define('action', {
+  title: {
+    type: Sequelize.STRING,
+  }
+});
+
+
+
+cmd.hasMany(action, {
+  foreignKey: 'actionID',
+});
+
+
+sequelize.sync();
+
+exports.sequelize = sequelize;
+exports.user = user;
+exports.course = course;
