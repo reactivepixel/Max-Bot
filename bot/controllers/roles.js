0211e9beecdbf94f1e70f6c5116ec410a5bd45b3
Chris Chapman <Chapman@apextion.com>
2017-08-16 17:06:23 -0400

    preping for pipeline build out

--- bot/controllers/roles.js
+++ bot/controllers/roles.js
@@ -1,19 +1,44 @@
 module.exports = () => {
 const util = require('apex-util');
 
-const disallowedRoles = ['admin', 'moderator'];
+const disallowedRoles = ['admin', 'moderator', 'tester'];
   messageMiddleware = (message) => {
     message.parsed = message.content.split(' ');
     return message;
   }
+
+  noGuildFault = (message) => {
+    return message.reply('Commands are Discord Server specific, they will not work in PMs. Sorry :cry:')
+  }
+
+  _roles = (message) => {
+    const msg = messageMiddleware(message);
+    if(msg.parsed[0].toLowerCase() === '!roles'){
+      const roles = [];
+      if(!message.guild) return noGuildFault(message);
+      message.guild.roles.map((role) => {
+        if(role.name != '@everyone'){
+          roles.push(role.name);
+        }
+      });
+
+      const formattedRole = roles.join('\n')
+
+      message.author.send('List of all Armada Roles: \n\n' + formattedRole);
+      return message.reply('Check your PMs :wink:');
+    }
+
+  }
+
   _addRole = (message) => {
     util.log('passed', message.content);
     const msg = messageMiddleware(message);
     if(msg.parsed[0].toLowerCase() === '!addrole'){
       // TODO: Reduce Code Duplication
+      if(!message.guild) return noGuildFault(message);
       let targetRole = message.guild.roles.find('name', msg.parsed[1])
       if(targetRole === null){
-        return message.reply('"' + msg.parsed[1] + '" is not a known role.')
+        return message.reply('"' + msg.parsed[1] + '" is not a known role. Try `!roles` to get a list of all Roles (The are case-sensitive)')
       }
       if (disallowedRoles.includes(targetRole.name.toLowerCase())){
         return message.reply('You are not worthy.')
@@ -29,9 +54,10 @@ const disallowedRoles = ['admin', 'moderator'];
     const msg = messageMiddleware(message);
     if(msg.parsed[0].toLowerCase() === '!removerole'){
       // TODO: Reduce Code Duplication
+      if(!message.guild) return noGuildFault(message);
       let targetRole = message.guild.roles.find('name', msg.parsed[1])
       if(targetRole === null){
-        return message.reply('"' + msg.parsed[1] + '" is not a known role.')
+        return message.reply('"' + msg.parsed[1] + '" is not a known role. Try `!roles` to get a list of all Roles (The are case-sensitive)')
       }
       if (disallowedRoles.includes(targetRole.name.toLowerCase())){
         return message.reply('You have not the power or the will to control this power.')
@@ -46,5 +72,6 @@ const disallowedRoles = ['admin', 'moderator'];
   return {
     addRole: _addRole,
     removeRole: _removeRole,
+    roles: _roles,
   }
 }
