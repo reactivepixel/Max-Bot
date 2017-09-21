102adc42894c17220955a01e12a4e07e2dd182c5
Mykah <mykah.taylor@gmail.com>
2017-09-21 15:15:13 -0400

    Adding paramater in welcome route

--- web/routes/root.js
+++ web/routes/root.js
@@ -2,8 +2,14 @@ const path = require('path');
 
 module.exports = (express) => {
   const router = express.Router();
-  router.get('/welcome', (req, res) => {
+  router.get('/welcome/:code', (req, res) => {
     res.sendFile(path.join(__dirname, '../views', 'welcome.html'));
+  });
+  router.get('/error', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views', 'error.html'));
+  });
+  router.get('/success', (req, res) => {
+    res.sendFile(path.join(__dirname, '../views', 'success.html'));
   });
   return router;
 };
