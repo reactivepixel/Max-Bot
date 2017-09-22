63d7fecc1912468c77db89d09218560723086b4f
Chris Chapman <Chapman@apextion.com>
2017-09-22 15:34:54 -0400

    Added Database Validation on email link click

--- web/routes/root.js
+++ web/routes/root.js
@@ -1,15 +1,41 @@
 const path = require('path');
+const models = require('../../db/models');
 
+const successRoute = '/success';
+const failRoute = '/error';
 
 module.exports = (express) => {
   const router = express.Router();
   router.get('/welcome/:code', (req, res) => {
-    res.sendFile(path.join(__dirname, '../views', 'welcome.html'));
+    // See if any Members match the supplied code
+    models.Member.find({
+      where: {
+        uuid: req.params.code,
+        verified: 0,
+      },
+    }).then((matchingMembers) => {
+      // Any Matches?
+      if (matchingMembers !== null) {
+        matchingMembers.update({
+          verified: 1,
+        }).then((member) => {
+          res.redirect(successRoute);
+        }).catch((err) => {
+          console.error(err);
+          res.redirect(failRoute);
+        });
+      } else {
+        res.redirect(failRoute);
+      }
+    }).catch((err) => {
+      console.error(err);
+      res.redirect(failRoute);
+    });
   });
-  router.get('/error', (req, res) => {
+  router.get(failRoute, (req, res) => {
     res.sendFile(path.join(__dirname, '../views', 'error.html'));
   });
-  router.get('/success', (req, res) => {
+  router.get(successRoute, (req, res) => {
     res.sendFile(path.join(__dirname, '../views', 'success.html'));
   });
   return router;
