0cff52a8a085c06c8b2315439e4f14d4ad8ed944
Rhuan Santos <rhuansantosdev@gmail.com>
2017-10-30 12:31:41 -0400

    comment broadcast.js

--- bot/controllers/broadcast.js
+++ bot/controllers/broadcast.js
@@ -6,13 +6,16 @@ module.exports = () => {
     const msg = Object.assign(message, container);
     return msg;
   };
+  // send message for all channels
   const _allChannels = (message) => {
     const msg = messageMiddleware(message);
     const input = msg.parsed.join(',').replace(/,/g, ' ');
     if (msg.parsed[0].toLowerCase() === '!broadcastall') {
       const channels = message.guild.channels.map(channel => channel);
       Object.keys(channels).forEach((el) => {
+        // filter for text channels
         if (channels[el].type === 'text') {
+          // creating an object to send a message
           const objEmbed = new Discord.RichEmbed()
             .setAuthor(message.author.tag, message.author.displayAvatarURL)
             .setTitle(`:loudspeaker: ${input.substr(input.indexOf(' ') + 1)}`)
@@ -24,10 +27,11 @@ module.exports = () => {
     }
     return false;
   };
+  // send message specifc channels
   const _chooseChannels = (message) => {
     const msg = messageMiddleware(message);
     const inputChannels = msg.parsed[1];
-
+    // delete the 2 first index of the array and replace `,` for blank space
     const input = msg.parsed.slice(1).slice(1).join(',').replace(/,/g, ' ');
     const validChannels = [];
     if (msg.parsed[0].toLowerCase() === '!broadcastchoose') {
@@ -38,6 +42,7 @@ module.exports = () => {
         }
         return true;
       });
+      // Verify if the user choose a valid channel
       const isValid = inputChannels.split(',').every(r => validChannels.includes(r));
       if (isValid) {
         inputChannels.split(',').forEach((el) => {
