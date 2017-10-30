d4d4c4fdaacadafa506a41e1196356ebef9f756d
Rhuan Santos <rhuansantosdev@gmail.com>
2017-10-30 02:25:01 -0400

    broadcasting choosing channels

--- bot/controllers/broadcast.js
+++ bot/controllers/broadcast.js
@@ -1,12 +1,10 @@
 module.exports = () => {
-  // const util = require('apex-util');
   const messageMiddleware = (message) => {
     const container = {};
     container.parsed = message.content.split(' ');
     const msg = Object.assign(message, container);
     return msg;
   };
-
   const _allChannels = (message) => {
     const msg = messageMiddleware(message);
     if (msg.parsed[0].toLowerCase() === '!broadcast-all') {
@@ -15,11 +13,26 @@ module.exports = () => {
         if (channels[el].type === 'text') {
           channels[el].send('sending it for every channel');
         }
+        return true;
       });
     }
-    return true;
+    return false;
+  };
+  const _chooseChannels = (message) => {
+    const msg = messageMiddleware(message);
+    if (msg.parsed[0].toLowerCase() === '!broadcast-choose') {
+      const channels = message.guild.channels.map(channel => channel);
+      Object.keys(channels).forEach((el) => {
+        if (channels[el].type === 'text') {
+          channels[2].send('sending message for a specific channel');
+        }
+        return true;
+      });
+    }
+    return false;
   };
   return {
     allChannels: _allChannels,
+    chooseChannels: _chooseChannels,
   };
 };
