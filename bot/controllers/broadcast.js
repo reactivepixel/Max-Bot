568998b1e84b82eb941f38a62f213ece5ed236d2
joegreen2 <allnations21@fullsail.edu>
2017-10-30 02:02:37 -0400

    broadcast bot storing input and send message
    broadcast bot storing input and send message
    not dynamic yet

--- bot/controllers/broadcast.js
+++ bot/controllers/broadcast.js
@@ -1,6 +1,5 @@
 module.exports = () => {
   const util = require('apex-util');
-
   const disallowedChannel = ['admin', 'moderator', 'tester', 'crew', 'fleet officer', '@everyone'];
   const messageMiddleware = (message) => {
     const container = {};
@@ -15,14 +14,28 @@ module.exports = () => {
     const msg = messageMiddleware(message);
     if (msg.parsed[0].toLowerCase() === '!broadcast') {
       const channels = [];
-      // const channelsSelected = msg.map();
+      // Store each channel in array, then loop through array and send message to each channel.
 
-      // const a = msg.map(chnl => chnl);
+      // if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+      const args = message.content.slice(message.length).split(/\s+/);
+      const command = args.shift().toLowerCase();
+
       util.log('----------------------STARTING LOGS--------------------');
       util.log(message);
-      util.log(msg.parsed);
+      util.log(args);
+      util.log(command);
+      util.log('----------------------Storing Msg--------------------');
+      // util.log(msg);
+      // util.log('#213134809172672512');
+      util.log('----------------------Array Storing--------------------');
+      // client.channels.get('#213134809172672512').send('My Message');
       util.log('----------------------ENDING LOGS--------------------');
+      // message.reply('My Message is this test!');
+      message.guild.channels.find('name', 'tabletopcitadel').sendMessage('blabla');
 
+
+      // -----------------------------------------------------
       if (!message.guild) return noGuildFault(message);
       message.guild.channels.map((channel) => {
         if (!disallowedChannel.includes(channel.name.toLowerCase())) {
