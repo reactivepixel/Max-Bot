64fbe113c7e163342807cacbc80a73d9c5ce0210
joegreen2 <allnations21@fullsail.edu>
2017-10-31 18:19:23 -0400

    bot command menu
    styled with examples, lives in controller, secret !limitless response
    (silly)

--- bot/controllers/botCommands.js
+++ bot/controllers/botCommands.js
@@ -6,41 +6,37 @@ module.exports = () => {
     const msg = Object.assign(message, container);
     return msg;
   };
-  const Discord = require('discord.js');
-  const client = new Discord.Client();
 
   const _commands = (message) => {
     const msg = messageMiddleware(message);
     if (msg.content.toLowerCase() === '!help') {
       message.reply({ embed: {
         color: 3447003,
-        // author: {
-        //   name: client.user.username,
-        //   icon_url: client.user.avatarURL,
-        // },
+        author: {
+          name: 'Max-Bot',
+        },
         title: 'Let the !help begin v1.2.3 Discovered Commands: ',
         url: 'https://github.com/joegreen2/Max-Bot',
-        description: 'The power of this Bot is **limitless!** Within the limits below:',
+        description: 'The power of this Bot is **!limitless** Within the limits below:',
         fields: [{
           name: '!broadCast',
-          value: 'USE: < !broadcast > command to send a message to selected channels.\nExample: (!broadcast channel1,channel2,channel3 Hello channels this is my message!) \n (seperate channels with \',\' and message starts after the space between channels message.)',
-          image: 'bot/img/IMG_1006.JPG',
+          value: 'USE: < !broadcast > command to send a message to selected channels.\n_Example: (!broadcast channel1,channel2,channel3 Hello channels this is my message!_) \n(seperate channels with  \' , \'  and message starts after the space between channels message.)',
         },
         {
           name: '!broadCastAll',
-          value: 'USE: < !broadcastall > command to send a message to all channels. \nExample: (!broadcastall To all my gamers: Leeroy Jenkins!)',
+          value: 'USE: < !broadcastall > command to send a message to all channels. \n_Example: (!broadcastall To all my gamers: Leeroy Jenkins!_)',
         },
         {
           name: '!roles',
           value: 'List all Armada Roles.',
         },
         {
-          name: '!addRole RoleName',
-          value: 'Adds yourself to a role and the related text/voice rooms. ',
+          name: '!addRole',
+          value: 'Adds yourself to a role and the related text/voice rooms. \n_Example: (!addRole RoleName)_',
         },
         {
-          name: '!removeRole RoleName',
-          value: 'Removes a role from yourself.',
+          name: '!removeRole',
+          value: 'Removes a role from yourself. \n_Example: (!removeRole RoleName)_',
         },
         {
           name: '!addAllRoles',
@@ -57,8 +53,7 @@ module.exports = () => {
         ],
         timestamp: new Date(),
         footer: {
-          // icon_url: client.user.avatarURL,
-          text: '© Example',
+          text: 'Max-Bot',
         },
       },
       });
