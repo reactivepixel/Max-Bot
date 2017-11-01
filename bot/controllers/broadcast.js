5a888c33df2c1a637dfc015e8e0bbaebff86f481
joegreen2 <allnations21@fullsail.edu>
2017-10-31 22:17:49 -0400

    change symbol to select multiple channels to broadcast message to
    went from ‘ , ‘ to  ‘ ++ ‘
    
    ‘ , ‘ is too common of a symbol used in grammar practices to use as a
    channel separator.

--- bot/controllers/broadcast.js
+++ bot/controllers/broadcast.js
@@ -48,9 +48,9 @@ module.exports = () => {
         }
       });
       // Get Message.
-      const input = msg.parsed.slice(1).slice(1).join(',').replace(/,/g, ' ');
+      const input = msg.parsed.slice(1).slice(1).join(' ');
       // Send message to channels.
-      chnls.split(',').forEach((chnl) => {
+      chnls.split('++').forEach((chnl) => {
         message.guild.channels.find('name', chnl).sendMessage(input);
       });
     }
