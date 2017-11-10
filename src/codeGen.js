9e7a3fef958272705fa0ad6bc0798984223437a9
LevanceWam <lkwamley@fullsail.edu>
2017-11-10 14:10:47 -0500

    Went back and got the code generators code up to eslint standrards
    Signed-off-by: LevanceWam <lkwamley@fullsail.edu>

--- src/codeGen.js
+++ src/codeGen.js
@@ -1,17 +1,17 @@
 // This is the code generator that will make the random code for user to type in.
-
 function codeGen() {
   const length = 6;
   const charset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let newCode = '';
-
-  for (i = 0, n = charset.length; i < length; i += 1) {
-    newCode = charset.charAt(Math.floor(Math.random() * n));
+  for (i = 0, n = charset.length; i < length; i++) {
+    newCode += charset.charAt(Math.floor(Math.random() * n));
   }
+  util.log(length);
   return newCode;
 }
 
 // where the code will be stored we have to send this to the user to verify.
 const secret = codeGen();
+util.log('this is the code: ' + secret);
 
 module.export.codeGen = codeGen;
