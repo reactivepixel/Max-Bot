759266c2ccf8385ac66da2f3e6f4b573e3a9ca62
Chris Chapman <Chapman@apextion.com>
2017-11-10 14:32:26 -0500

    added generation of db/config/config

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,8 @@ gulp.task('db', () => {
   require('dotenv').config();
   const config = {
     development: {
-      username: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASS,
+      username: 'root',
+      password: process.env.MYSQL_ROOT_PASSWORD,
       database: process.env.MYSQL_DATABASE,
       host: process.env.MYSQL_HOST,
       dialect: 'mysql',
