a7e6db6e1e5d460896288971122c6770eb70a718
Mark Langenhorst <mlangenhorst@fullsail.edu>
2018-01-19 00:56:19 -0600

    adding channels.js for !announce functionality in new code format

---
+++ bot/controllers/channels.js
@@ -0,0 +1,58 @@
+const BaseController = require('../baseController.js');
+const Command = require('../baseCommand.js');
+const util = require('apex-util');
+
+class ChannelController extends BaseController {
+  constructor(message) {
+    super(message);
+    const controller = this;
+    this.commands = [
+      new Command('!channels', '!channels', 'List All Channels', 'List all available Channels', this.channelsAction.bind(controller), 'dm'),
+      new Command('!announce', '!announce <channel_name>, <message>', 'Announce to other channels', 'Broadcast to multiple channels. Channels are case-sensitive.', this.announceAction.bind(controller)),
+    ];
+  }
+
+  channelsAction() {
+    const { message } = this;
+    const channels = [];
+    message.guild.channels.map((channel) => {
+        return channels.push(channel.name);
+      return channel.name;
+    });
+    return 'List of all Armada Channels: \n\n' + channels.join('\n');
+  }
+
+  announceAction() {
+    const { message } = this;
+    const channels = message.parsed[1].split(',');
+    util.log('Multiple Channels Parsing', channels, 4);
+
+    channels.map((channel) => {
+        const targetChannel = message.guild.channels.find('name', channel);
+        util.log('Asking API for Channel', targetChannel, 4);
+
+        if (targetChannel === null) {
+          return '"' + channel + '" is not a known channel. Try `!channels` to get a list of all Channels (They are case-sensitive)';
+        }
+
+        // Set parsed value to 2 for message.
+        let msgParsedIndex = 2;
+        let preparedMessage = '';
+
+        // Loop through/join user message by space until end.
+        while (msgParsedIndex < message.parsed.length) {
+          // Add spaces after first word
+          if (msgParsedIndex !== 2) {
+            preparedMessage += ' ';
+          }
+          preparedMessage += message.parsed[msgParsedIndex];
+          msgParsedIndex += 1;
+        }
+        return targetChannel.send(preparedMessage);
+      });
+
+    return 'Broadcast sent!';
+  }
+}
+
+module.exports = ChannelController;
