42f2566a261cb17c23708534981f3718bb24d1f0
Kloe Rogers <kloerogers@Kloes-MacBook-Pro.local>
2018-03-04 14:59:17 -0500

    Added the message for a new user with their username included

--- bot/client.js
+++ bot/client.js
@@ -57,4 +57,10 @@ client.on('message', (message) => {
   }
 });
 
+// Send message for a new user
+client.on('guildMemberAdd', (member) => {
+  util.log('THE MEMBER', member.user.username, 0);
+  member.sendMessage(`Welcome to the server ${member.user.username}!. Enjoy your stay!`);
+});
+
 client.login(process.env.TOKEN);
