34040a0a15adc5e648bb13667bb4ea63b022c5e2
LevanceWam <lkwamley@fullsail.edu>
2018-03-04 19:13:26 -0500

    Added the welcome.js and figured out why welcome.js wasnt working was exporting with () instead of =

--- bot/controllers/welcome.js
+++ bot/controllers/welcome.js
@@ -1,1 +1,32 @@
-// Levance Wamley
+const BaseController = require('../baseController.js');
+const Command = require('../baseCommand.js');
+
+
+class WelcomeController extends BaseController {
+  constructor(message) {
+    // Call BaseController constructor
+    super(message);
+
+    // Aliasing 'this' as controller to allow for binding in actions
+    const controller = this;
+
+    // Array of all commands, see baseCommand.js for prototype
+    this.commands = [
+      new Command(
+        '!welcome',
+        '!welcome',
+        'Welcome Message',
+        'Resend the welcome message you recieved when you joined the server.',
+        this.welcomeAction.bind(controller),
+        'dm',
+      ),
+    ];
+  }
+
+  welcomeAction() {
+    const { message } = this;
+    return `Welcome to the server, ${message.server}`;
+  }
+}
+
+module.exports = WelcomeController;
