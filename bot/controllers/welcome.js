b3e9670160e6f1c0d06d4593fcc3458defc0d801
BHess2653 <GM_Dahaka@BHMac.local>
2018-03-04 20:50:16 -0500

    created and started welcome.js

---
+++ bot/controllers/welcome.js
@@ -0,0 +1,24 @@
+const BaseController = require('../baseController.js');
+const Command = require('../baseCommand.js');
+
+class WelcomeController extends BaseController {
+  constructor(message) {
+    // Call BaseController constructor
+    super(message);
+
+    // Aliasing 'this' as controller to allow for binding in actions
+    const controller = this;
+
+    // Array of all commands, see baseCommand.js for prototype
+    this.commands = [
+      new Command(
+        '!welcome',
+        '!welcome',
+        'Welcome Message',
+        'Replay welcome message recieved when you join the server.',
+        this.removeAllRolesAction.bind(controller),
+      ),
+    ];
+}
+
+module.exports = WelcomeController;
