847e38ed1a88739e8ce8314051a7307fd24f1366
Kloe Rogers <kloerogers@Kloes-MacBook-Pro.local>
2018-03-09 12:14:26 -0500

    Able to bring back top users with a dynamic limit. Can not return them yet.

---
+++ bot/controllers/topPoints.js
@@ -0,0 +1,39 @@
+const BaseController = require('../baseController.js');
+const Command = require('../baseCommand.js');
+const models = require('../../db/models');
+const util = require('apex-util');
+
+class topPointsController extends BaseController {
+  constructor(message) {
+    super(message);
+    const controller = this;
+    this.commands = [
+      new Command(
+        '!topUsers',
+        '!topUsers <Limit_Wanted>',
+        'Return Top Number of Users',
+        'Sends a message with the users with the most points earned.',
+        this.topPointsAction.bind(controller),
+        'dm',
+      ),
+    ];
+  }
+
+  async topPointsAction() {
+    const { message } = this;
+    util.log('Limit Wanted', message.parsed[1], 4);
+    if (!isNaN(message.parsed[1])) {
+      const users = await models.Member.findAll(
+        {
+          where: { verified: 1 },
+          limit: parseInt(message.parsed[1], 10),
+          order: [['points', 'DESC']],
+        },
+      );
+      util.log('TRYING', users[0].email, 4);
+      return users || 'nope';
+    }
+    return 'Is that a number? You might want to try again.';
+  }
+}
+module.exports = topPointsController;
