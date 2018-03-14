5b27968095e238ec9c38649f42282810e1dbc7c7
Michael  Sewell <MikeSewell01@gmail.com>
2018-03-13 20:18:04 -0400

    added messageCount to the members table

---
+++ db/migrations/add-column-memberCount.js
@@ -0,0 +1,9 @@
+
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.addColumn('Members', 'messagesCount', {
+    type: Sequelize.INTEGER,
+  }),
+
+  down: (queryInterface, Sequelize) => queryInterface.removeColumn('Members', 'messagesCount'),
+};
