2a6a93fa4e7cc5a09edae02ecda15aa2a9e4bac8
Kloe Rogers <kloerogers@Kloes-MacBook-Pro.local>
2018-03-16 18:29:41 -0400

    Removed all traces of messagesCount from the files

--- db/migrations/add-column-memberCount.js
+++
@@ -1,8 +0,0 @@
-
-module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.addColumn('Members', 'messagesCount', {
-    type: Sequelize.INTEGER,
-  }),
-
-  down: (queryInterface, Sequelize) => queryInterface.removeColumn('Members', 'messagesCount'),
-};
