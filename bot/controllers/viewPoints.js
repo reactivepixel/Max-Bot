4674f5fb9757def73f2a80c4e2f9fb79dd03237d
Daniel Rubio <drubio432@gmail.com>
2018-03-17 16:31:40 -0700

    updated syntax for style guide

--- bot/controllers/viewPoints.js
+++ bot/controllers/viewPoints.js
@@ -1,7 +1,8 @@
 const BaseController = require('../baseController.js');
 const Command = require('../baseCommand.js');
-const util = require('apex-util');
-const { Member } = require('../../db/models');
+const {
+  Member,
+} = require('../../db/models');
 
 class ViewPointsController extends BaseController {
   constructor(message) {
@@ -20,12 +21,17 @@ class ViewPointsController extends BaseController {
   }
 
   async viewPointsAction() {
-      const {message} = this;
-      // Get Member Data
-      const memberData = await Member.findAll({ attributes: ['points'], where: { discordUser: message.author.id } });
-      // Get points
-      const points = memberData[0].dataValues.points;
-      message.author.send(`Your total Points: ${points}`);
+    const { message } = this;
+    // Get Member Data
+    const memberData = await Member.findAll({
+      attributes: ['points'],
+      where: {
+        discordUser: message.author.id,
+      },
+    });
+    // Get points
+    const points = memberData[0].dataValues.points;
+    message.author.send(`Your total Points: ${points}`);
   }
 }
 
