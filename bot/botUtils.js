acc442ad269415d3342da8510e9f9ad906d96635
Michael  Sewell <MikeSewell01@gmail.com>
2018-03-17 01:46:46 -0400

    added email utils function

--- bot/botUtils.js
+++ bot/botUtils.js
@@ -1,4 +1,6 @@
 const { Member } = require('../db/models');
+const nodemailer = require('nodemailer');
+const util = require('apex-util');
 
 exports.generateCode = (n) => {
   // Workaround method for Math.pow() and ** operator
@@ -39,4 +41,42 @@ exports.getUserPointsandUpdate = async (userId, pointsToAdd) => {
   await Member.update(
     { points: memberData[0].dataValues.points + pointsToAdd },
     { where: { discordUser: userId } });
+};
+
+
+// Nodemailer email function
+// Set up Nodemailer to send emails through gmail
+exports.sendEmail = async (message, toEmailAdd, emailSubject,
+  emailBody, emailSendType, callback) => {
+  const sendInvite = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL_USERNAME,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+  // Nodemailer email recipient & message
+  // the email template
+  const mailOptions = {
+    from: process.env.EMAIL_USERNAME,
+    to: toEmailAdd,
+    subject: emailSubject,
+    html: emailBody,
+  };
+  // Call sendMail on sendInvite
+  // Pass mailOptions & callback function
+  sendInvite.sendMail(mailOptions, (err, info) => {
+    let messageTypeString = 'send an invite.';
+    emailSendType === 'verify' ? messageTypeString = 'verify' : '';
+    const errorMsg = `Oops, looks like the email can not be sent. It's not you, it's me. Please reach out to a moderator to help you ${messageTypeString}.`;
+    if (err) {
+      message.reply(errorMsg);
+      util.log('Email not sent', err, 3);
+      callback(false);
+    } else {
+      util.log('Email details', info, 3);
+      callback(true);
+    }
+  });
+  return null;
 };
