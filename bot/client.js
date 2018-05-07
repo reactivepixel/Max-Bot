12d67cba4419d02eac0ba6afb0b6bdfefdece362
Matt <mattpezzente@gmail.com>
2018-05-07 00:41:42 -0400

    Added general chat welcome to new users

--- bot/client.js
+++ bot/client.js
@@ -15,9 +15,12 @@ client.on('ready', () => {
   util.log('Bot Online and Ready', 0);
 });
 
-// Message new guild members
+// Actions for new guild members
 client.on('guildMemberAdd', (member) => {
-  member.sendMessage('Welcome to the server! Please read our Terms of Service below to get started: \n\n --Terms of Service--');
+  // Send a the direct welcome message to a guild member
+  member.send('Welcome to the server! Please read our Terms of Service below to get started: \n\n --Terms of Service--');
+  // Welcome a new user in the general chat
+  client.channels.get('442823511036395541').send(`Welcome to Armada ${member.user.username}!`);
   util.log('Message Sent to New Member', 0);
 });
 
