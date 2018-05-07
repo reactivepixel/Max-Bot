5e7facd412d9603f87b5cf05c64b6fca2a8eb027
BHess2653 <GM_Dahaka@BHMac.local>
2018-05-07 02:46:32 -0400

    fixed eslint error on welcome.js

--- bot/controllers/welcome.js
+++ bot/controllers/welcome.js
@@ -1,6 +1,5 @@
 const BaseController = require('../baseController.js');
 const Command = require('../baseCommand.js');
-const util = require('apex-util');
 
 class WelcomeController extends BaseController {
   constructor(message) {
