342e9baf205afc0b2631ce050bbc8d74a28a2a6f
James Taber <JATaber@fullsail.edu>
2018-05-20 17:24:55 -0500

    setting up a layout for commands

--- bot/controllers/games.js
+++ bot/controllers/games.js
@@ -3,4 +3,19 @@ const Command = require('../baseCommand.js');
 const util = require('apex-util');
 
 class GamesController extends BaseController {
+  constructor(message) {
+    super(message);
+    const controller = this;
+
+    this.commands = [
+      new Command(
+        '!8ball',
+      ),
+      new Command(
+        'coinFlip',
+      ),
+    ];
+  }
 }
+
+module.exports = GamesController;
