ee47613eac6bf4de69a3f5042927541b997f392e
James Taber <JATaber@fullsail.edu>
2018-05-20 13:23:51 -0500

    started to set up the feature

---
+++ bot/controllers/games.js
@@ -0,0 +1,6 @@
+const BaseController = require('../baseController.js');
+const Command = require('../baseCommand.js');
+const util = require('apex-util');
+
+class GamesController extends BaseController {
+}
