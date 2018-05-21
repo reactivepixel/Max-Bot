2ac1102204d167eb96896ce77f49d1a21bb2bd7d
BHess2653 <GM_Dahaka@BHMac.local>
2018-05-20 22:53:59 -0400

    Updated to work with new JSON changes

--- bot/controllers/channels.js
+++ bot/controllers/channels.js
@@ -12,7 +12,7 @@ class ChannelController extends BaseController {
         '!channels',
         '!channels',
         'List All Channels',
-        msg.channels.channelHelpMsg[process.env.LANGUAGE],
+        msg.channels.helpMsg[process.env.LANGUAGE],
         this.channelsAction.bind(controller),
         'dm',
       ),
@@ -20,7 +20,7 @@ class ChannelController extends BaseController {
         '!announce',
         '!announce <channel_name>,[channel_name] <message>',
         'Announce To Channels',
-        msg.announce.announceHelpMsg[process.env.LANGUAGE],
+        msg.announce.helpMsg[process.env.LANGUAGE],
         this.announceAction.bind(controller),
         'reply',
         true,
@@ -32,7 +32,7 @@ class ChannelController extends BaseController {
     const { message } = this;
     const channels = [];
     message.guild.channels.map(channel => channels.push(channel.name));
-    return msg.channels.channelMsg[process.env.LANGUAGE] + channels.join('\n');
+    return msg.channels.msg[process.env.LANGUAGE] + channels.join('\n');
   }
 
   announceAction() {
@@ -46,7 +46,7 @@ class ChannelController extends BaseController {
       util.log('Asking API for Channel', targetChannel, 4);
 
       if (targetChannel === null) {
-        return '"' + channel + '" ' + msg.channels.channelError[process.env.LANGUAGE];
+        return '"' + channel + '" ' + msg.channels.errorMsg[process.env.LANGUAGE];
       }
 
       // Set parsed value to 2 for message.
@@ -62,10 +62,10 @@ class ChannelController extends BaseController {
         preparedMessage += message.parsed[msgParsedIndex];
         msgParsedIndex += 1;
       }
-      return targetChannel.send(sender + msg.announce.announceMsg[process.env.LANGUAGE] + preparedMessage + '```');
+      return targetChannel.send(sender + msg.announce.msg[process.env.LANGUAGE] + preparedMessage + '```');
     });
 
-    return msg.announce.announceMsgReturn[process.env.LANGUAGE];
+    return msg.announce.msgReturn[process.env.LANGUAGE];
   }
 }
 
