04032d72e076836d53d7a750a6589bfa592b7057
BHess2653 <GM_Dahaka@BHMac.local>
2018-05-20 22:53:42 -0400

    Updated to work with new JSON changes

--- bot/controllers/verify.js
+++ bot/controllers/verify.js
@@ -21,7 +21,7 @@ class VerifyController extends BaseController {
         '!verify',
         '!verify <email_address>',
         'Verify Email Address',
-        msg.verify.verifyHelpMsg[process.env.LANGUAGE],
+        msg.verify.helpMsg[process.env.LANGUAGE],
         this.verifyAction.bind(controller),
       ),
     ];
