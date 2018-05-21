c9169c3e33cbe9beb2ad6af8e9d541c44d0ad2d3
James Taber <JATaber@fullsail.edu>
2018-05-21 03:23:49 -0500

    trying to get dadJoke to store outside the https request

--- bot/controllers/games.js
+++ bot/controllers/games.js
@@ -75,13 +75,13 @@ class GamesController extends BaseController {
     const req = https.request(options, (res) => {
       res.on('data', (data) => {
         // parse the data returned
-        const body = JSON.parse(data);
+        const body = JSON.parse(data.joke);
         // push the joke into the variable
-        joke.push(body.joke);
+        joke.push(body);
       });
     });
     req.end();
-    return `Here is your joke ${message.author.username}: ${joke}`;
+    return `Here is your joke ${message.author.username}: ${JSON.stringify(joke[0])}`;
   }
 }
 
