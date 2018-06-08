9beb030269247a41386d7abe3cebf8652d24ad26
Cody Maxwell <codemax999@live.com>
2018-06-08 18:41:29 -0400

    Welcome Users

--- bot/client.js
+++ bot/client.js
@@ -17,9 +17,13 @@ client.on('ready', () => {
 
 // Listen for messages
 client.on('message', (message) => {
+  // set content to !tos if new memeber
+  const newMsg = message;
+  newMsg.content = message.type === 'GUILD_MEMBER_JOIN' ? '!tos' : message.content;
+
   // Check for ! prefix on message to ensure it is a command
-  if (message.content.charAt(0) === '!') {
-    util.log('Command message received', message.content, 0);
+  if (newMsg.content.charAt(0) === '!') {
+    util.log('Command message received', newMsg.content, 0);
 
     // Build basic help string
     let helpString = 'v1.4.0 Discovered Commands:\n\n\t**<> - Required Item\t\t[] - Optional Item**';
@@ -27,20 +31,20 @@ client.on('message', (message) => {
     // Process message against every controller
     Object.keys(controllers).forEach((key) => {
       // Instantiate the controller
-      const controllerInstance = new controllers[key](message);
+      const controllerInstance = new controllers[key](newMsg);
       util.log('Controller instance', controllerInstance, 5);
       // Runs commands after constructor is called
       controllerInstance.run();
 
       // Loop through commands if help command and add to string
-      if (message.content.toLowerCase() === '!help') {
+      if (newMsg.content.toLowerCase() === '!help') {
         Object.keys(controllerInstance.commands).forEach((commandKey) => {
           const commandInstance = controllerInstance.commands[commandKey];
           // Check if command should be shown in help menu
           if (commandInstance.showWithHelp) {
-            if (commandInstance.adminOnly && isAdmin(message.member)) {
+            if (commandInstance.adminOnly && isAdmin(newMsg.member)) {
               helpString += `\n\n \`${commandInstance.example}\` **- Admin Only** \n\t ${commandInstance.description}`;
-            } else if (commandInstance.adminOnly && !isAdmin(message.member)) {
+            } else if (commandInstance.adminOnly && !isAdmin(newMsg.member)) {
               helpString += '';
             } else {
               helpString += `\n\n \`${commandInstance.example}\` \n\t ${commandInstance.description}`;
@@ -51,10 +55,17 @@ client.on('message', (message) => {
     });
 
     // If help command called, display string
-    if (message.content.toLowerCase() === '!help') {
-      message.reply(helpString);
+    if (newMsg.content.toLowerCase() === '!help') {
+      newMsg.reply(helpString);
     }
   }
+});
+
+// Listen for new users to join
+client.on('guildMemberAdd', (member) => {
+  // Display welcome message to new memeber in General Channel
+  const general = member.guild.channels.find('name', 'general');
+  general.send(`Greetings, ${member}. I've been expecting you.`);
 });
 
 client.login(process.env.TOKEN);
