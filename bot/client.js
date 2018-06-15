c831387b3042bb1028ad908f1c3ab24ec387e576
wsf235 <wsf235@users.noreply.github.com>
2018-06-15 13:45:54 -0400

    Add files via upload

--- bot/client.js
+++ bot/client.js
@@ -18,13 +18,13 @@ client.on('ready', () => {
 // Listen for messages
 client.on('message', (message) => {
 
-  //create a ToS message for new members
-  const new_message = message;
-  new_message.content = message.type == 'GUILD_MEMBER_JOIN' ? '!terms' : new_message.content;
+  // Create a ToS message for new members
+  const newMessage = message;
+  newMessage.content = message.type === 'GUILD_MEMBER_JOIN' ? '!terms' : new_message.content;
 
   // Check for ! prefix on message to ensure it is a command
-  if (new_message.content.charAt(0) === '!') {
-    util.log('Command message received', new_message.content, 0);
+  if (newMessage.content.charAt(0) === '!') {
+    util.log('Command message received', newMessage.content, 0);
 
     // Build basic help string
     let helpString = 'v1.4.0 Discovered Commands:\n\n\t**<> - Required Item\t\t[] - Optional Item**';
@@ -32,20 +32,20 @@ client.on('message', (message) => {
     // Process message against every controller
     Object.keys(controllers).forEach((key) => {
       // Instantiate the controller
-      const controllerInstance = new controllers[key](new_message);
+      const controllerInstance = new controllers[key](newMessage);
       util.log('Controller instance', controllerInstance, 5);
       // Runs commands after constructor is called
       controllerInstance.run();
 
       // Loop through commands if help command and add to string
-      if (new_message.content.toLowerCase() === '!help') {
+      if (newMessage.content.toLowerCase() === '!help') {
         Object.keys(controllerInstance.commands).forEach((commandKey) => {
           const commandInstance = controllerInstance.commands[commandKey];
           // Check if command should be shown in help menu
           if (commandInstance.showWithHelp) {
-            if (commandInstance.adminOnly && isAdmin(new_message.member)) {
+            if (commandInstance.adminOnly && isAdmin(newMessage.member)) {
               helpString += `\n\n \`${commandInstance.example}\` **- Admin Only** \n\t ${commandInstance.description}`;
-            } else if (commandInstance.adminOnly && !isAdmin(new_message.member)) {
+            } else if (commandInstance.adminOnly && !isAdmin(newMessage.member)) {
               helpString += '';
             } else {
               helpString += `\n\n \`${commandInstance.example}\` \n\t ${commandInstance.description}`;
@@ -56,8 +56,8 @@ client.on('message', (message) => {
     });
 
     // If help command called, display string
-    if (new_message.content.toLowerCase() === '!help') {
-      new_message.reply(helpString);
+    if (newMessage.content.toLowerCase() === '!help') {
+      newMessage.reply(helpString);
     }
   }
 });
@@ -65,14 +65,15 @@ client.on('message', (message) => {
 // Listen out for new users to the discord
 client.on('guildMemberAdd', (member) => {
   // Add new welcome message to general
-  const main_channel = member.guild.channels.find('name', 'general');
-  main_channel.send(`Hi, ${member}. It's nice to meet you!`);
+  const mainChannel = member.guild.channels.find('name', 'general');
+  mainChannel.send(`Hi, ${member}. It's nice to meet you!`);
 });
 
-//Listen out for members that leave the discord
+// Listen out for members that leave the discord
 client.on('guildMemberRemove', (member) => {
-  const main_channel = member.guild.channels.find('name', 'general');
-  main_channel.send(`${member} has left... it was nice knowing you! ;-;`);
+  // Add a message for someone leaving the discord
+  const mainChannel = member.guild.channels.find('name', 'general');
+  mainChannel.send(`${member} has left... it was nice knowing you! ;-;`);
 });
 
 client.login(process.env.TOKEN);
