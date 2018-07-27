4b4e6735287d581495d15572e99af538f10fd170
KankuroDev <kankuro865@gmail.com>
2018-07-27 18:19:51 -0500

    Added regex and an additional conditional for dice format checking. Edited command description to be valid. Edited error message to be valid.

--- bot/controllers/dice.js
+++ bot/controllers/dice.js
@@ -16,7 +16,7 @@ class DiceController extends BaseController {
         '!roll',
         '!roll <roll_count><die_type>,[roll_count][die_type]',
         'Rolls the dice',
-        'Rolls dice based on the user input. Must be in #D# format. Example: 1D20 rolls a single D20.',
+        'Rolls dice based on the user input. Must be in #D# format for multiple rolls or D# for a single roll. Example: 2D20 rolls two D20; D20 rolls one D20.',
         this.rollAction.bind(controller),
       ),
     ];
@@ -30,15 +30,21 @@ class DiceController extends BaseController {
     let total = 0;
     let rollsString = '';
     let invalidDie = false;
+    const diceFormat = /(\d*)?d(?:(\d+|F))(?:([+-]\d*))?/; // Regex to verify correct dice format
 
     diceCommand.map((singleDie) => {
       // Convert string to lowercase
       const singleDieLowercase = singleDie.toLowerCase();
 
-      // Checks if the user has a 'd' and then if there is a number before and after the 'd'
-      if (singleDieLowercase.includes('d') && (singleDieLowercase.indexOf('d') - 1) > -1 && (singleDieLowercase.substr(singleDieLowercase.indexOf('d') + 1))) {
-        // Get the specifics of the roll based on the location of the 'd'
-        let rollCount = singleDieLowercase.substr(0, singleDieLowercase.indexOf('d'));
+      // Check if the user has a 'd' and if there is only numberic values before and after the 'd'
+      if (singleDieLowercase.match(diceFormat) && !isNaN(singleDieLowercase.substr(0, singleDieLowercase.indexOf('d')))) {
+        let rollCount = 1;
+
+        // Check how many to roll, if no number is given keep default of 1
+        if (singleDieLowercase.substr(0, singleDieLowercase.indexOf('d'))) {
+          rollCount = singleDieLowercase.substr(0, singleDieLowercase.indexOf('d'));
+        }
+        // Get the sides and name of the die
         const sides = singleDieLowercase.substr(singleDieLowercase.indexOf('d') + 1);
         const dieName = 'D' + sides; // Name of the current dice
 
@@ -66,7 +72,7 @@ class DiceController extends BaseController {
       // Return results
       return results;
     } else {
-      return 'The dice you entered is invalid, please make sure to use the proper format **#D#**!';
+      return 'The dice you entered is invalid, please make sure to use the proper format of **#D#** for multiple rolls or **D#** for a single roll!';
     }
   }
 }
