a7dbb194a90f329150823032d1758b747fb6a39a
KankuroDev <kankuro865@gmail.com>
2018-07-27 17:26:40 -0500

    Fixed out of memory error. Added conditional to check for specific formatting. Added error output if input wasn’t correct.

--- bot/controllers/dice.js
+++ bot/controllers/dice.js
@@ -29,30 +29,45 @@ class DiceController extends BaseController {
     util.log('Parsed message: ', diceCommand, 4);
     let total = 0;
     let rollsString = '';
+    let invalidDie = false;
 
     diceCommand.map((singleDie) => {
-      // Get the specifics of the roll based on the location of the 'd'
-      let rollCount = singleDie.toLowerCase().substr(0, singleDie.indexOf('d'));
-      const sides = singleDie.toLowerCase().substr(singleDie.indexOf('d') + 1);
-      const dieName = 'D' + sides; // Name of the current dice
+      // Convert string to lowercase
+      const singleDieLowercase = singleDie.toLowerCase();
 
-      // Loop for the amount of rolls needed
-      while (rollCount !== 0) {
-        // Perform the random number selection
-        const rollNumber = Math.floor((Math.random() * sides) + 1);
-        rollsString += dieName + ': ' + rollNumber + '\n';
-        rollCount -= 1;
+      // Checks if the user has a 'd' and then if there is a number before and after the 'd'
+      if (singleDieLowercase.includes('d') && (singleDieLowercase.indexOf('d') - 1) > -1 && (singleDieLowercase.substr(singleDieLowercase.indexOf('d') + 1))) {
+        // Get the specifics of the roll based on the location of the 'd'
+        let rollCount = singleDieLowercase.substr(0, singleDieLowercase.indexOf('d'));
+        const sides = singleDieLowercase.substr(singleDieLowercase.indexOf('d') + 1);
+        const dieName = 'D' + sides; // Name of the current dice
 
-        // Add roll to the total
-        total += rollNumber;
+        // Loop for the amount of rolls needed
+        while (rollCount !== 0) {
+          // Perform the random number selection
+          const rollNumber = Math.floor((Math.random() * sides) + 1);
+          rollsString += dieName + ': ' + rollNumber + '\n';
+          rollCount -= 1;
+          total += rollNumber;
+        }
+        return rollsString;
+      } else {
+        // Set the invalidDie to true
+        invalidDie = true;
+        return invalidDie;
       }
-      return rollsString;
     });
-    // Create the results string
-    const results = 'here are your results! \n' + rollsString + '\n**Total: ' + total + '!**';
 
-    // Return results
-    return results;
+    // Check if input was invalid
+    if (invalidDie === false) {
+      // Create the results string if input was valid
+      const results = 'here are your results! \n' + rollsString + '\n**Total: ' + total + '!**';
+
+      // Return results
+      return results;
+    } else {
+      return 'The dice you entered is invalid, please make sure to use the proper format **#D#**!';
+    }
   }
 }
 
