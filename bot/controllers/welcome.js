701a19a9435068ab6ec94abf17aee46de19d5020
Malcolm Ross <MJRoss@student.fullsail.edu>
2018-08-13 20:37:56 -0400

    Added Welcome controller.
    This controller contains a command that will resend the welcome message to a user in the form of a direct message.

---
+++ bot/controllers/welcome.js
@@ -0,0 +1,25 @@
+const BaseController = require('../baseController.js');
+const Command = require('../baseCommand.js');
+
+class WelcomeController extends BaseController {
+  constructor(message) {
+    super(message);
+    const controller = this;
+    this commands = [
+      new Command(
+        '!resendWelcome',
+        '!resendWelcome',
+        'Resend Welcome Message',
+        'Sends a direct message to the user with the original welcome message.',
+        this.welcomeAction.bind(controller),
+        'dm',
+      ),
+    ];
+  }
+
+  welcomeAction() {
+    const { message } = this;
+    return `Welcome to the server ${message.member}!\r\nYou can view the terms of use here!`;
+  }
+}
+module.exports = WelcomeController;
